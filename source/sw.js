2150f89188b2f25c4f333cdcbaa1281c992ead6a
yousero <yousero.art@gmail.com>
2021-05-30 23:35:50 +0500

    refactor sw.js

--- source/sw.js
+++ source/sw.js
@@ -1,10 +1,10 @@
-var CACHE_VERSION = 1
-var CURRENT_CACHES = {
+const CACHE_VERSION = 1
+const CURRENT_CACHES = {
   'read-through': 'read-through-cache-v' + CACHE_VERSION
 }
 
 self.addEventListener('activate', function (event) {
-  var expectedCacheNames = Object.keys(CURRENT_CACHES).map(function (key) {
+  const expectedCacheNames = Object.keys(CURRENT_CACHES).map(function (key) {
     return CURRENT_CACHES[key]
   })
 
@@ -13,7 +13,6 @@ self.addEventListener('activate', function (event) {
       return Promise.all(
         cacheNames.map(function (cacheName) {
           if (expectedCacheNames.indexOf(cacheName) === -1) {
-            console.log('Deleting out of date cache:', cacheName)
             return caches.delete(cacheName)
           }
         })
@@ -28,23 +27,11 @@ self.addEventListener('fetch', function (event) {
       .match(event.request)
       .then(function (response) {
         if (response) {
-          console.log(' Found response in cache:', response)
-
           return response
         }
-        console.log(
-          ' No response for %s found in cache. ' +
-            'About to fetch from network...',
-          event.request.url
-        )
         return fetch(event.request.clone())
       })
       .then(function (response) {
-        console.log(
-          '  Response for %s from network is: %O',
-          event.request.url,
-          response
-        )
         if (response.status < 400) {
           cache.put(event.request, response.clone())
         }
